58c268c37e54a470c841c3ef5ecb7722f625552e
Kazu Yamamoto <kazu@iij.ad.jp>
2011-02-08 22:52:34 +0900

    text and lazy text.

---
+++ Text/Parsec/Text.hs
@@ -0,0 +1,37 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Text.Parsec.Text
+-- Copyright   :  (c) Kazu Yamamoto 2010
+-- License     :  BSD-style (see the LICENSE file)
+--
+-- Maintainer  :  derek.a.elkins@gmail.com
+-- Stability   :  provisional
+-- Portability :  portable
+--
+-- Make strict Texts an instance of 'Stream' with 'Char' token type.
+--
+-----------------------------------------------------------------------------
+
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+module Text.Parsec.Text ( 
+    Parser, GenParser, parseFromFile
+  ) where
+
+import Text.Parsec.Error
+import Text.Parsec.Prim
+
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+
+instance (Monad m) => Stream T.Text m Char where
+    uncons = return . T.uncons
+
+type Parser = Parsec T.Text ()
+type GenParser t st = Parsec T.Text st
+
+parseFromFile :: Parser a -> String -> IO (Either ParseError a)
+parseFromFile p fname = do
+    input <- T.readFile fname
+    return (runP p () fname input)
